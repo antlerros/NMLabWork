26388f94194d88f3e052dd8c20d1698266f3e680
acnokego <b02901115@ntu.edu.tw>
2016-10-09 22:19:03 +0800

    function of TicTacToe

--- TicTacToeDemo/app/src/main/java/com/example/antlerros95/tictactoedemo/PlayGame.java
+++ TicTacToeDemo/app/src/main/java/com/example/antlerros95/tictactoedemo/PlayGame.java
@@ -1,10 +1,157 @@
 package com.example.antlerros95.tictactoedemo;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
 
 /**
  * Created by antlerros95 on 10/5/16.
  */
 
 public class PlayGame extends Activity {
+    private int times_left;
+    private Button b[][];
+    private int c[][];
+    private static boolean Turn = true;
+    private int player1_score = 0;
+    private int player2_score = 0;
+    private TextView score1;
+    private TextView score2;
+
+    class MyClickListener implements View.OnClickListener {
+        int x;
+        int y;
+
+
+        public MyClickListener(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+
+        public void onClick(View view) {
+            if (b[x][y].isEnabled()) {
+                b[x][y].setEnabled(false);
+                if (Turn) {
+                    b[x][y].setText("O");
+                    c[x][y] = 1;
+                } else {
+                    b[x][y].setText("X");
+                    c[x][y] = 2;
+                }
+                if(checkboard()){
+                    times_left = times_left -1;
+                    score1.setText(Integer.toString(player1_score));
+                    score2.setText(Integer.toString(player2_score));
+                    if(times_left==0){
+                        switch_to_start_menu();
+                    }
+                    else setBoard();
+                }
+                Turn ^= true;
+            }
+        }
+    }
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_game);
+
+        Bundle bundle = this.getIntent().getExtras();
+        times_left = bundle.getInt("times");
+
+
+        setBoard();
+
+
+
+    }
+
+    private void setBoard() {
+        b = new Button[3][3];
+        c = new int[3][3];
+        score1 = (TextView)findViewById(R.id.score1);
+        score2 = (TextView)findViewById(R.id.score2);
+
+        b[0][0] = (Button) findViewById(R.id.button1);
+        b[0][1] = (Button) findViewById(R.id.button2);
+        b[0][2] = (Button) findViewById(R.id.button3);
+        b[1][0] = (Button) findViewById(R.id.button4);
+        b[1][1] = (Button) findViewById(R.id.button5);
+        b[1][2] = (Button) findViewById(R.id.button6);
+        b[2][0] = (Button) findViewById(R.id.button7);
+        b[2][1] = (Button) findViewById(R.id.button8);
+        b[2][2] = (Button) findViewById(R.id.button9);
+
+
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                b[i][j].setOnClickListener(new MyClickListener(i,j));
+                if(!b[i][j].isEnabled()) {
+                    b[i][j].setText(" ");
+                    b[i][j].setEnabled(true);
+                }
+                c[i][j] = 0;
+
+            }
+        }
+
+
+    }
+
+    private boolean checkboard(){
+        boolean gameOver = false;
+        if ((c[0][0] == 1 && c[1][1] == 1 && c[2][2] == 1)
+                || (c[0][2] == 1 && c[1][1] == 1 && c[2][0] == 1)
+                || (c[0][1] == 1 && c[1][1] == 1 && c[2][1] == 1)
+                || (c[0][2] == 1 && c[1][2] == 1 && c[2][2] == 1)
+                || (c[0][0] == 1 && c[0][1] == 1 && c[0][2] == 1)
+                || (c[1][0] == 1 && c[1][1] == 1 && c[1][2] == 1)
+                || (c[2][0] == 1 && c[2][1] == 1 && c[2][2] == 1)
+                || (c[0][0] == 1 && c[1][0] == 1 && c[2][0] == 1)) {
+
+            player1_score++;
+            gameOver = true;
+        } else if ((c[0][0] == 2 && c[1][1] == 2 && c[2][2] == 2)
+                || (c[0][2] == 2 && c[1][1] == 2 && c[2][0] == 2)
+                || (c[0][1] == 2 && c[1][1] == 2 && c[2][1] == 2)
+                || (c[0][2] == 2 && c[1][2] == 2 && c[2][2] == 2)
+                || (c[0][0] == 2 && c[0][1] == 2 && c[0][2] == 2)
+                || (c[1][0] == 2 && c[1][1] == 2 && c[1][2] == 2)
+                || (c[2][0] == 2 && c[2][1] == 2 && c[2][2] == 2)
+                || (c[0][0] == 2 && c[1][0] == 2 && c[2][0] == 2)) {
+
+            player2_score++;
+            gameOver = true;
+        } else {
+            boolean empty = false;
+            for(int i=1; i<=3; i++) {
+                for(int j=1; j<=3; j++) {
+                    if(c[i][j]==0) {
+                        empty = true;
+                        break;
+                    }
+                }
+            }
+            if(!empty) {
+                gameOver = true;
+            }
+        }
+        return gameOver;
+
+    }
+
+    private void switch_to_start_menu() {
+        Intent _intent = new Intent();
+        _intent.setClass(PlayGame.this, StartGame.class);
+        startActivity(_intent);
+
+    }
+
 }
